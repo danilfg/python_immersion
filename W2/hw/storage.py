93714fefa5026a1106b228cab0a207af289d792f
danilfg <danilfg@yahoo.com>
2020-04-23 16:04:21 +0300

    'donehw1'

--- W2/hw/storage.py
+++ W2/hw/storage.py
@@ -3,27 +3,35 @@ import tempfile
 import argparse
 import json
 
+data_dict = {}
+
 storage_path = os.path.join(tempfile.gettempdir(), "storage.data")
-with open(storage_path, 'r') as data_file:
-	data_dict = json.load(data_file)
+if os.path.isfile(storage_path):
+    with open(storage_path, 'r') as data_file:
+        data_dict = json.load(data_file)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--key", action="store")
 parser.add_argument("--value", action="store")
 args = parser.parse_args()
-#temp_dict[args.key] = [args.value]
+
+str = ''
 
 if args.value == None:
-	print(data_dict[args.key])
+    if args.key not in data_dict:
+        print("")
+    else:
+        for i in range(len(data_dict[args.key])):
+            str += data_dict[args.key][i] + ", "
+        str = str[0:-2]
+        print(str)
 else:
 	if args.key not in data_dict:
 		data_dict[args.key] = [args.value]
 	else:
 		data_dict[args.key].append(args.value)
 
-#json.dump(data_dict, open(storage_path, 'w'))
-
-print(data_dict)
+json.dump(data_dict, open(storage_path, 'w'))
 
 	
 
