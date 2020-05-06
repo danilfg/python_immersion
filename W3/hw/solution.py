f8cdfd00165a03743cda25d7118c125fb8648933
danilfg <danilfg@yahoo.com>
2020-05-06 17:29:24 +0300

    test2

--- W3/hw/solution.py
+++ W3/hw/solution.py
@@ -1,50 +1,65 @@
 import csv
 import os
 
-ext_list = [".jpg", ".jpeg", ".png", ".gif"]
-
-
 class CarBase:
-    def __init__(self, brand, carrying, photo_file_name=None):
+
+    def __init__(self, brand, photo_file_name, carrying):
+
+        ext_list = [".jpg", ".jpeg", ".png", ".gif"]
+
         self.brand = brand
-        self.photo_file_name = photo_file_name
-        self.carrying = carrying
+        if self.get_photo_file_ext() in ext_list:
+            self.photo_file_name = photo_file_name
+        try:
+            self.carrying = float(carrying)
+        except ValueError:
+            pass
 
     def get_photo_file_ext(self):
         return os.path.splitext(self.photo_file_name)[1]
 
 
 class Car(CarBase):
+
+    car_type = 'car'
+
     def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
-        super().__init__(brand, photo_file_name, carrying)
+        super(Car, self).__init__(brand, photo_file_name, carrying)
         try:
-            self.passenger_seats_count = passenger_seats_count
-        except Exception:
+            self.passenger_seats_count = int(passenger_seats_count)
+        except ValueError:
             pass
 
 
 class Truck(CarBase):
+
+    car_type = 'truck'
+
     def __init__(self, brand, photo_file_name, carrying, body_whl):
-        super().__init__(brand, photo_file_name, carrying)
-        param = body_whl.split("x")
-        try:
-            self.body_length = float(param[0])
-            self.body_width = float(param[1])
-            self.body_height = float(param[2])
-        except Exception:
-            pass
+        super(Truck, self).__init__(brand, photo_file_name, carrying)
+        if body_whl:
+            param = body_whl.split("x")
+            if len(param) == 3:
+                self.body_length = float(param[0])
+                self.body_width = float(param[1])
+                self.body_height = float(param[2])
+            else:
+                self.body_length = self.body_width = self.body_height = 0.0
+        else:
+            self.body_length = self.body_width = self.body_height = 0.0
 
     def get_body_volume(self):
         return self.body_length * self.body_width * self.body_height
 
 
 class SpecMachine(CarBase):
+
+    car_type = 'spec_machine'
+
     def __init__(self, brand, photo_file_name, carrying, extra):
-        super().__init__(brand, photo_file_name, carrying)
-        try:
-            self.extra = extra
-        except Exception:
-            pass
+        super(SpecMachine, self).__init__(brand, photo_file_name, carrying)
+        self.extra = extra
+
 
 def get_car_list(csv_filename):
     with open(csv_filename, encoding='utf-8') as csv_fd:
