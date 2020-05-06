884b1817d855a93be3e10abc5d3c4dd2d525d33e
danilfg <danilfg@yahoo.com>
2020-05-06 17:07:40 +0300

    test

--- W3/hw/solution.py
+++ W3/hw/solution.py
@@ -5,8 +5,7 @@ ext_list = [".jpg", ".jpeg", ".png", ".gif"]
 
 
 class CarBase:
-    def __init__(self, car_type, brand, carrying, photo_file_name=None):
-        self.car_type = car_type
+    def __init__(self, brand, carrying, photo_file_name=None):
         self.brand = brand
         self.photo_file_name = photo_file_name
         self.carrying = carrying
@@ -16,8 +15,8 @@ class CarBase:
 
 
 class Car(CarBase):
-    def __init__(self, car_type, brand, photo_file_name, carrying, passenger_seats_count):
-        super().__init__(car_type, brand, photo_file_name, carrying)
+    def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
+        super().__init__(brand, photo_file_name, carrying)
         try:
             self.passenger_seats_count = passenger_seats_count
         except Exception:
@@ -25,8 +24,8 @@ class Car(CarBase):
 
 
 class Truck(CarBase):
-    def __init__(self, car_type, brand, photo_file_name, carrying, body_whl):
-        super().__init__(car_type, brand, photo_file_name, carrying)
+    def __init__(self, brand, photo_file_name, carrying, body_whl):
+        super().__init__(brand, photo_file_name, carrying)
         param = body_whl.split("x")
         try:
             self.body_length = float(param[0])
@@ -40,8 +39,8 @@ class Truck(CarBase):
 
 
 class SpecMachine(CarBase):
-    def __init__(self, car_type, brand, photo_file_name, carrying, extra):
-        super().__init__(car_type, brand, photo_file_name, carrying)
+    def __init__(self, brand, photo_file_name, carrying, extra):
+        super().__init__(brand, photo_file_name, carrying)
         try:
             self.extra = extra
         except Exception:
@@ -55,11 +54,11 @@ def get_car_list(csv_filename):
         for row in reader:
             try:
                 if row[0] == "car":
-                    car_list.append(Car(row[0], row[1], row[5], row[3], row[2]))
+                    car_list.append(Car(row[1], row[5], row[3], row[2]))
                 if row[0] == "truck":
-                    car_list.append(Truck(row[0], row[1], row[5], row[3], row[4]))
+                    car_list.append(Truck(row[1], row[5], row[3], row[4]))
                 if row[0] == "spec_machine":
-                    car_list.append(SpecMachine(row[0], row[1], row[5], row[3], row[6]))
+                    car_list.append(SpecMachine(row[1], row[5], row[3], row[6]))
             except Exception:
                 pass
 
--- W3/hw/solution_car.py
+++ W3/hw/solution_car.py
@@ -6,8 +6,7 @@ ext_list = [".jpg", ".jpeg", ".png", ".gif"]
 
 
 class CarBase:
-    def __init__(self, car_type, brand, carrying, photo_file_name=None):
-        self.car_type = car_type
+    def __init__(self, brand, carrying, photo_file_name=None):
         self.brand = brand
         self.photo_file_name = photo_file_name
         self.carrying = carrying
@@ -17,8 +16,8 @@ class CarBase:
 
 
 class Car(CarBase):
-    def __init__(self, car_type, brand, photo_file_name, carrying, passenger_seats_count):
-        super().__init__(car_type, brand, photo_file_name, carrying)
+    def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
+        super().__init__(brand, photo_file_name, carrying)
         try:
             self.passenger_seats_count = passenger_seats_count
         except ValueError:
@@ -26,8 +25,8 @@ class Car(CarBase):
 
 
 class Truck(CarBase):
-    def __init__(self, car_type, brand, photo_file_name, carrying, body_whl):
-        super().__init__(car_type, brand, photo_file_name, carrying)
+    def __init__(self, brand, photo_file_name, carrying, body_whl):
+        super().__init__(brand, photo_file_name, carrying)
         param = body_whl.split("x")
         try:
             self.body_length = float(param[0])
@@ -41,8 +40,8 @@ class Truck(CarBase):
 
 
 class SpecMachine(CarBase):
-    def __init__(self, car_type, brand, photo_file_name, carrying, extra):
-        super().__init__(car_type, brand, photo_file_name, carrying)
+    def __init__(self, brand, photo_file_name, carrying, extra):
+        super().__init__(brand, photo_file_name, carrying)
         try:
             self.extra = extra
         except ValueError:
@@ -56,11 +55,11 @@ def get_car_list(csv_filename):
         for row in reader:
             try:
                 if row[0] == "car":
-                    car_list.append(Car(row[0], row[1], row[5], row[3], row[2]))
+                    car_list.append(Car(row[1], row[5], row[3], row[2]))
                 if row[0] == "truck":
-                    car_list.append(Truck(row[0], row[1], row[5], row[3], row[4]))
+                    car_list.append(Truck(row[1], row[5], row[3], row[4]))
                 if row[0] == "spec_machine":
-                    car_list.append(SpecMachine(row[0], row[1], row[5], row[3], row[6]))
+                    car_list.append(SpecMachine(row[1], row[5], row[3], row[6]))
             except:
                 pass
 
